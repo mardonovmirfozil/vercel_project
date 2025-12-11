24f069978619eaba90222d7e82d31edaeb5079f0
mardonovmirfozil <mirfozilmardonov15@gmail.com>
2025-12-11 15:51:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+        perspiciatis, quod atque, itaque ex eveniet eos, omnis et cum officia
+        hic reiciendis voluptate expedita nulla. Magni vel illum temporibus
+        voluptates
+      </h1>
+    </>
+  );
+}
+
+export default App;
